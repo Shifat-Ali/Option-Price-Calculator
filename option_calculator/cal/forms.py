9ee0ef93267422dee42ccf8e403ec797a5c25830
Shifat Ali <a.shifat@iitg.ac.in>
2023-07-09 14:04:53 +0530

    Add files via upload

---
+++ option_calculator/cal/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+#creating form
+class two_stepForm(forms.Form):
+    stock_price = forms.DecimalField()
+    spot_price = forms.DecimalField()
+    risk_free_interest_rate = forms.DecimalField()
+    maturity_time = forms.DecimalField(help_text='in months')
+    standard_deviation = forms.DecimalField(help_text='in % per annum')
+
+class n_stepForm(forms.Form):
+    steps = forms.IntegerField()
+    stock_price = forms.DecimalField()
+    spot_price = forms.DecimalField()
+    risk_free_interest_rate = forms.DecimalField()
+    maturity_time = forms.DecimalField(help_text='in months')
+    standard_deviation = forms.DecimalField(help_text='in % per annum')
+
+class black_scholesForm(forms.Form):
+    stock_price = forms.DecimalField()
+    spot_price = forms.DecimalField()
+    standard_deviation = forms.DecimalField(help_text='in % per annum')
+    risk_free_interest_rate = forms.DecimalField()
+    maturity_time = forms.DecimalField(help_text='in months')
+    
